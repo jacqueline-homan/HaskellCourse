25eb2142d03f3fd838007f1ac1df98e893dc1c72
jacqueline-homan <jacquelinehoman7@gmail.com>
2016-10-28 19:14:53 -0400

    composition

---
+++ composition.hs
@@ -0,0 +1,4 @@
+a = 1
+b = 2 * a + 1
+f a b = a + b
+g a b = 2 * (f a b)
--- fold.hs
+++ fold.hs
@@ -4,3 +4,5 @@ foldedList2 = foldr (+) 0 listNums
 
 showPlus s x = "(" ++ s ++ "+" ++ (show x) ++ ")"
 --in GHCi, run ` foldl showPlus "0" [1,2,3,4]`
+showPlus' x s = "(" ++ (show x) ++ "+" ++ s ++ ")"
+--in GHCi, run ` foldr showPlus' "0" listNums
