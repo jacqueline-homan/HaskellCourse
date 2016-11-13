7847e8c2d98e6826b9efd75f2ed7ea4501292006
jacqueline-homan <jacquelinehoman7@gmail.com>
2016-11-13 01:57:10 -0500

    parameterized types example

--- algebraic_data_types.hs
+++ algebraic_data_types.hs
@@ -5,11 +5,19 @@ data Customer = Customer CustomerId String Int
 
 data Response = Yes | No | Help | Quit
 
+data MaybeInt = NoInt | JustInt Int
+
+defaultInt :: Int -> MaybeInt -> Int
+defaultInt defaultVal NoInt = defaultVal
+defaultInt _ (JustInt x) = x
+
 alice :: Customer
 alice = Customer (MakeCustomerId 13) "Alice" 42
 
 getCustomerId :: Customer -> CustomerId
 getCustomerId (Customer cust_id _ _) = cust_id
+
+
 
 {-For some reason, this don't work:
 
@@ -29,3 +37,15 @@ data Client = GovOrg String
             | Company String Integer String String
             | Individual String String Bool
             deriving Show
+
+data StringList = EmptyStringList
+                | ConsStringList String StringList
+
+lengthStringList :: StringList -> Int
+lengthStringList EmptyStringList = 0
+lengthStringList (ConsStringList _ xs) = 1 + lengthStringList xs
+{- This function above is the same as this function here: 
+length :: [a] -> Int
+length [] = 0
+length (_ : xs) = 1 + length xs
+-}
---
+++ parameterized_types.hs
@@ -0,0 +1,13 @@
+-- Definition of the Maybe type from the Haskell standard library:
+data Maybe a = Just a | Nothing
+-- Definition of the List type from the Haskell standard library
+data List a = Empty | Cons a (List a)
+
+data Map k a = ...
+
+x :: Maybe Int
+x = Nothing
+
+fromMaybe :: a -> Maybe a -> a
+fromMaybe defaultVal Nothing = defaultVal
+fromMaybe _ (Just x) = x
